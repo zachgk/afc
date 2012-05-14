a6195af366891eb2c713699b8d6159ee954ff795
zachgk <zachary@kimberg.com>
2012-05-14 17:13:20 -0500

    Basic framework for updating views per charity on drupal field

--- Server Code/ad.php
+++ Server Code/ad.php
@@ -14,7 +14,8 @@
     window.addEventListener('load',pw_load,false);}})();
 </script>
 
-<?php switch($_GET["ad_number"]) {
+<?php 
+switch($_GET["ad_number"]) {
 	case 1:
 		print '<div id="pw_adbox_60904_4_0"></div>';
 		break;
@@ -31,4 +32,11 @@
 		print '<div id="pw_adbox_63814_4_0"></div>';
 		break;
 }
+
+if($charity = $_GET["charity"]){
+  $add_view_query = "UPDATE dr_field_data_field_charity_views SET field_charity_views_value=field_charity_views_value+1 WHERE entity_id=$charity";
+  //$add_view_result = mysqli_query(/*database connect variable*/,$add_view_query);
+  //Should I be changing dr_field_revision_field_charity_views instead?
+}
+
  ?>
