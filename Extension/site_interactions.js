84d399049546537e78809d52162cc33dce28ab59
zachgk <zachary@kimberg.com>
2012-05-14 15:59:26 -0500

    Set up charity selection interaction

--- Extension/site_interactions.js
+++ Extension/site_interactions.js
@@ -1,1 +1,4 @@
 console.log('Interact with the ads4chairty.org site as needed');
+$('.select-charity').click(function(e){
+  chrome.extension.sendRequest({action: "set_charity", "charity": $(this).attr('data-cid')});
+});
