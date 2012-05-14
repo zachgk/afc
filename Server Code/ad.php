794f3c7ec227a4585989ebe861e63332ee6dce49
dan-silver <dannysilver3@gmail.com>
2012-05-13 21:25:03 -0500

    project wonderfull ad code instead of sample

--- Server Code/ad.php
+++ Server Code/ad.php
@@ -1,6 +1,17 @@
-<?php
-//This file should be called to send the ad - currently it sends a red sample ad
-//It should also take the input of user and increase their number of ad views.
-?>
-
-<img src="http://afc.zachkimberg.com/samplead.png"></img>
+<script type="text/javascript">
+   (function(){function pw_load(){
+      if(arguments.callee.z)return;else arguments.callee.z=true;
+      var d=document;var s=d.createElement('script');
+      var x=d.getElementsByTagName('script')[0];
+      s.type='text/javascript';s.async=true;
+      s.src='//www.projectwonderful.com/pwa.js';
+      x.parentNode.insertBefore(s,x);}
+   if (window.attachEvent){
+    window.attachEvent('DOMContentLoaded',pw_load);
+    window.attachEvent('onload',pw_load);}
+   else{
+    window.addEventListener('DOMContentLoaded',pw_load,false);
+    window.addEventListener('load',pw_load,false);}})();
+</script><!-- End Project Wonderful Ad Box Loader --><!-- Project Wonderful Ad Box Code -->
+<div id="pw_adbox_60904_4_0">
+  &nbsp;</div>
