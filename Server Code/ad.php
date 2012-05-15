94bc08d10b5ebb9f6500ef145f1df8a98ff45878
zachgk <zachary@kimberg.com>
2012-05-14 21:53:44 -0500

    Prevent attacks on ad.php - minorly

--- Server Code/ad.php
+++ Server Code/ad.php
@@ -33,7 +33,7 @@ switch($_GET["ad_number"]) {
 		break;
 }
 
-if($charity = $_GET["charity"]){
+if( ($charity = $_GET["charity"]) && is_numberic($charity)){
   $mysqli = new mysqli("localhost", "zachki5_drplafc", "2weeks", "zachki5_drplafc");
   $add_view_query = "UPDATE dr_field_data_field_charity_views SET field_charity_views_value=field_charity_views_value+1 WHERE entity_id=$charity";
   $mysqli->query($add_view_query);
