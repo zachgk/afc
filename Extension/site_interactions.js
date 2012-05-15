29cc441de1907991216a33487df539951d6e1fc8
zachgk <zachary@kimberg.com>
2012-05-14 23:09:02 -0500

    Change Personal Contribution on website

--- Extension/site_interactions.js
+++ Extension/site_interactions.js
@@ -1,3 +1,23 @@
+var background_local_storage;
+
+function rewrite_personal_contributions(){
+	$('span.a4c_personal_contribution').each(function(index, element){
+	  var total_raised = $(element).attr('data-raised').substr(1);
+	  var cid = $(element).parent().parent().find('.select-charity').attr('data-cid');
+	  var personal_views = background_local_storage['charity-' + cid ];
+	  if(typeof personal_views === "undefined") personal_views = 0;
+	  var total_charity_views = $(element).attr('data-views')
+	  var contribution = total_raised * personal_views / total_charity_views;
+	  contribution = Math.round(100*contribution)/100;
+	  $(element).html('$' + contribution);
+	});
+}
+
+chrome.extension.sendRequest({action: "get_local_storage"}, function(response){
+  background_local_storage = response.localStorage;
+  rewrite_personal_contributions();
+});
+
 console.log('Interact with the ads4chairty.org site as needed');
 $('.select-charity').click(function(e){
   chrome.extension.sendRequest({action: "set_charity", "charity": $(this).attr('data-cid')});
