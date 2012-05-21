27c46a9070581f2c9aeb1f81a4ca038f510164b1
Dan Silver <dannysilver3@gmail.com>
2012-05-20 23:57:40 -0500

    First step at a better positioning system for the advertisements.  They can now be docked to the right and left side of the screen (which corresponds to their absolute position)

--- Extension/drag.js
+++ Extension/drag.js
@@ -1,15 +1,17 @@
-// JavaScript Document
-
-var wk ,wk2, ad_number=1, charity_selection, mouseX;
+var wk ,wk2, charity_selection, mouseX;
+var ad_number=1;
 
 function enable_drag(){
   $('.a4c_ad').draggable({
     "containment":"body",
 	"handle":".a4c_move",
 	"stop":function(event, ui){
-	  save_ad_positions();
 	  $(this).addClass('a4c_idle');
 	  disable_guides();
+	  if ($(this).css("left").substring(0, $(this).css("left").length - 2) > $(window).width() / 2) {
+		$(this).css("right", $(window).width() - $(this).css("left").substring(0, $(this).css("left").length - 2) - 62.5).css("left", "auto");
+	  }
+	  save_ad_positions();
 	},
 	"start":function(event,ui){
       enable_guides();
@@ -34,7 +36,7 @@ function enable_remove(){
 function save_ad_positions(){
   wk = new Array();
   $('.a4c_ad').each(function(){
-    wk2 ={"top": $(this).css('top') ,"left": $(this).css('left')};
+	wk2 ={"top": $(this).css('top') ,"left": $(this).css('left'), "right": $(this).css('right')};
     wk.push( wk2 );
   });
   chrome.extension.sendRequest({action: "save_ad_positions", "positions": wk, "url": window.location.host});
@@ -42,8 +44,10 @@ function save_ad_positions(){
 
 function create_ad() {
   if(ad_number <= 5){
-	start_ad(  (ad_number * 130) + "px"  ,"100px");
-/*	chrome.extension.sendRequest({action: "display_message", message: "Add number " + ad_number + " has been created.", type: "alert", time: 2000}, function(response){
+	start_ad(  (ad_number * 130) + "px"  ,"100px", "auto");
+/*	
+	//Debug code for message numbers and an example of using our noty helper functions for displaying messages
+	chrome.extension.sendRequest({action: "display_message", message: "Add number " + ad_number + " has been created.", type: "alert", time: 2000}, function(response){
 		noty(response.formated_message);
 	});
 */
@@ -56,9 +60,9 @@ function create_ad() {
 
 
 
-function start_ad(top,left) {
+function start_ad(top,left, right) {
 	//we need to return different embed code for each ad, so ad_number will be an integer between 1 and 5
-	$('body').append("<div class='a4c_ad a4c_ad_new a4c_idle' style='top: "+top+"; left: "+left+";'><embed src='http://ads4charity.org/ad.php?ad_number="+ad_number+"&charity="+charity_selection+"'><div class='a4c_panel'><span class='a4c_remove'>Remove</span><br><span class='a4c_move'>Move</span></div></div>");
+	$('body').append("<div class='a4c_ad a4c_ad_new a4c_idle' style='top: "+top+"; left: "+left+"; right: " + right + "'><embed src='http://ads4charity.org/ad.php?ad_number="+ad_number+"&charity="+charity_selection+"'><div class='a4c_panel'><span class='a4c_remove'>Remove</span><br><span class='a4c_move'>Move</span></div></div>");
 		$(".a4c_ad_new").disableSelection(); //prevents users from highlighting the panel/advertisement
 		$(".a4c_ad").hover(function(){
 			$(this).find(".a4c_panel").css({
@@ -79,8 +83,8 @@ function start_ad(top,left) {
 }
 
 function enable_guides() {
-	$("#a4c_guide_box").show();
 	update_guides();
+	$("#a4c_guide_box").show();
 }
 
 function disable_guides() {
@@ -126,7 +130,7 @@ function startup(){
 
 function startup_ads(data){
   for(i=0; i<data.positions.length; i++){
-    start_ad(data.positions[i].top,data.positions[i].left);
+    start_ad(data.positions[i].top,data.positions[i].left, data.positions[i].right);
   }
 }
 
