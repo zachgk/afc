eaee64845c3184e263ac954ec819a1b0be354c11
Dan Silver <dannysilver3@gmail.com>
2012-05-22 09:53:19 -0500

    Option for Clearing Advertisement Positions
    CSS was also added to handle settings that use input buttons.

--- Extension/fancy-settings/source/manifest.js
+++ Extension/fancy-settings/source/manifest.js
@@ -4,8 +4,8 @@ this.manifest = {
     "icon": "icon48.png",
     "settings": [
         {
-            "tab": "Automatic Advertisment Placement",
-            "group": "Ad Placement Templates",
+            "tab": "Advertisements",
+            "group": "Automatic Advertisment Placement",
             "name": "autoAds",
             "type": "radioButtons",
             "label": "Once you move an advertisement, the template will be overridden for only that website. <br>To return to the default template or switch templates, remove all advertisements on that website.<br>",
@@ -19,11 +19,26 @@ this.manifest = {
 			]
 		},
 		{
+            "tab": "Advertisements",
+            "group": "Clear Custom Advertisement Positions",
+            "name": "remove_description",
+            "type": "description",
+            "text": 'This setting clears any advertisements that you have manually placed.<br>If you are using a template, all websites will now use it again.'
+        },
+		{
+            "tab": "Advertisements",
+            "group": "Clear Custom Advertisement Positions",
+            "name": "remove_all",
+            "type": "button",
+            "text": "Clear Custom Advertisement Positions",
+       },
+		{
             "tab": "Charities",
             "group": "Charity Selection",
             "name": "charities",
             "type": "description",
             "text": 'To choose a charity that you want your advertisement views to count towards, visit <a href="http://www.ads4charity.org/charities"> our website </a>.'
         }
-	]
+	],
+	    
 };
--- Extension/fancy-settings/source/settings.js
+++ Extension/fancy-settings/source/settings.js
@@ -1,60 +1,8 @@
 window.addEvent("domready", function () {
-    // Option 1: Use the manifest:
     new FancySettings.initWithManifest(function (settings) {
-     //   settings.manifest.autoAds.addEvent("action", function () {
-	//		console.log(this);
-     //   });
+        settings.manifest.remove_all.addEvent("action", function () {
+		  chrome.extension.sendRequest({action: "remove_all_ads"});
+		  console.log("clearing ads sent");
+        });
     });
-    
-    // Option 2: Do everything manually:
-    /*
-    var settings = new FancySettings("My Extension");
-    
-    var username = settings.create({
-        "tab": i18n.get("information"),
-        "group": i18n.get("login"),
-        "name": "username",
-        "type": "text",
-        "label": i18n.get("username"),
-        "text": i18n.get("x-characters")
-    });
-    
-    var password = settings.create({
-        "tab": i18n.get("information"),
-        "group": i18n.get("login"),
-        "name": "password",
-        "type": "text",
-        "label": i18n.get("password"),
-        "text": i18n.get("x-characters-pw"),
-        "masked": true
-    });
-    
-    var myDescription = settings.create({
-        "tab": i18n.get("information"),
-        "group": i18n.get("login"),
-        "name": "myDescription",
-        "type": "description",
-        "text": i18n.get("description")
-    });
-    
-    var myButton = settings.create({
-        "tab": "Information",
-        "group": "Logout",
-        "name": "myButton",
-        "type": "button",
-        "label": "Disconnect:",
-        "text": "Logout"
-    });
-    
-    // ...
-    
-    myButton.addEvent("action", function () {
-        alert("You clicked me!");
-    });
-    
-    settings.align([
-        username,
-        password
-    ]);
-    */
 });
