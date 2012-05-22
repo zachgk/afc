d47c64c22008209797f992fbf4abf3a181ad0ea5
Dan Silver <dannysilver3@gmail.com>
2012-05-22 10:15:56 -0500

    Removed console logs

--- Extension/fancy-settings/source/settings.js
+++ Extension/fancy-settings/source/settings.js
@@ -2,7 +2,6 @@ window.addEvent("domready", function () {
     new FancySettings.initWithManifest(function (settings) {
         settings.manifest.remove_all.addEvent("action", function () {
 		  chrome.extension.sendRequest({action: "remove_all_ads"});
-		  console.log("clearing ads sent");
         });
     });
 });
