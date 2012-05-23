eb094713b651bcd446c9a421e216da859e3ed993
Dan Silver <dannysilver3@gmail.com>
2012-05-23 11:25:10 -0500

    Options page improvements
    Disabled selection on options page and added a generic "your settings
    have been saved" for all checkboxes

--- Extension/fancy-settings/source/settings.js
+++ Extension/fancy-settings/source/settings.js
@@ -1,6 +1,7 @@
 (function($) {
 	$(function() {
 		new FancySettings.initWithManifest(function (settings) {
+			$("html").css("-webkit-user-select", "none");
 			$("#remove_all").click(function() {
 				chrome.extension.sendRequest({action: "remove_all_ads"});
 				chrome.extension.sendRequest({action: "display_message", message: "Advertisements will now use the selected template.  If no template is selected, advertisements will not appear on any website until you create them.", time: 4000}, function(response){
@@ -9,6 +10,11 @@
 			});
 			$(".autoAds").click(function() {
 				chrome.extension.sendRequest({action: "display_message", message: "Advertisements are now automatically being placed with the <i>" + $(this).next().text() + "</i> template.", time: 4000}, function(response){
+					noty(response.formated_message);
+				});
+			});
+			$("label.checkbox").click(function() {
+				chrome.extension.sendRequest({action: "display_message", message: "Your settings have been saved."}, function(response){
 					noty(response.formated_message);
 				});
 			});
