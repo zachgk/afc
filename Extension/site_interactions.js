9cdf67c36f1bc1b88009719d159736921a8cc51e
zachgk <zachary@kimberg.com>
2012-05-24 13:44:13 -0500

    site_interactions: Hide, show, and add dashboard link

--- Extension/site_interactions.js
+++ Extension/site_interactions.js
@@ -1,2 +1,6 @@
 console.log('Interact with the ads4chairty.org site as needed');
 
+$('.a4c_extension_show').show();
+$('.a4c_extension_hide').hide();
+
+$('#topMenu .links.main-menu li.menu-371').after('<li><a href="/dashboard" title="View the Dashboard">Dashboard</a></li>');
