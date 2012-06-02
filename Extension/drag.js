ddb002affa7ae2f5eb72386df21c2b2d5b1f3e3e
Dan Silver <dannysilver3@gmail.com>
2012-06-02 08:27:05 -0700

    On manual removal of all ads, blacklist site
    The hide ads button in the popup and the new case of manually removing
    ads both call the blacklist_current_site() in background.html

--- Extension/drag.js
+++ Extension/drag.js
@@ -36,6 +36,9 @@ function enable_remove() {
 	ad_codes.push($(this).parent().parent().attr("afc_ad_id"));
 	ad_number--;
     $(this).parent().parent().fadeOut().remove();
+	if (ad_number == 1) {
+	  chrome.extension.sendRequest({action: "blacklist_current_site", "url": url });
+	}
 	save_ad_positions();
   });
 }
